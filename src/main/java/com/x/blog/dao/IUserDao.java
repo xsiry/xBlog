4adbd5e32298e0742d94e12653263bf5801dfee9
xsiry <xsiries@gmail.com>
2017-04-21 11:14:05 +0800

    #update

--- src/main/java/com/x/blog/dao/IUserDao.java
+++ src/main/java/com/x/blog/dao/IUserDao.java
@@ -1,7 +1,14 @@
 package com.x.blog.dao;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.x.blog.pojo.User;
+
 /**
  * Created by xsiry on 2017/4/21.
  */
-public class IUserDao {
+public interface IUserDao {
+    //接口方法，通过用户名得到User对象
+    User checkLogin(@Param("userName") String name, @Param("passWord") String password);
 }
+
--- src/main/java/com/x/blog/pojo/User.java
+++ src/main/java/com/x/blog/pojo/User.java
@@ -4,4 +4,34 @@ package com.x.blog.pojo;
  * Created by xsiry on 2017/4/21.
  */
 public class User {
+
+    private int sysusid;
+    private String username;
+    private String pswd;
+    private int nethoid;
+
+    public int getId() {
+        return sysusid;
+    }
+    public void setId(int sysusid) {
+        this.sysusid = sysusid;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return pswd;
+    }
+    public void setPassword(String password) {
+        this.pswd = password;
+    }
+    public int getNethoid() {
+        return nethoid;
+    }
+    public void setNethoid(int nethoid) {
+        this.nethoid = nethoid;
+    }
 }
